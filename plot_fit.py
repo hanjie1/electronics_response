91240057a82bf9fbc0960aafc2d9bdc2b59397b1
hanjie1 <liu28238@gmail.com>
2023-03-15 17:06:55 -0400

    good functions

--- plot_fit.py
+++ plot_fit.py
@@ -6,23 +6,26 @@ from scipy.stats import chi2
 
 runno=15155
 
-def ResFunc(x, par0, par1):
+def ResFunc(x, par0, par1, par2, par3):
+
+    xx = x-par2
+
     A1 = 4.31054*par0
     A2 = 2.6202*par0
     A3 = 0.464924*par0
     A4 = 0.762456*par0
     A5 = 0.327684*par0
 
-    E1 = np.exp(-2.94809*x/par1)
-    E2 = np.exp(-2.82833*x/par1)
-    E3 = np.exp(-2.40318*x/par1)
+    E1 = np.exp(-2.94809*xx/par1)
+    E2 = np.exp(-2.82833*xx/par1)
+    E3 = np.exp(-2.40318*xx/par1)
 
-    lambda1 = 1.19361*x/par1
-    lambda2 = 2.38722*x/par1
-    lambda3 = 2.5928*x/par1
-    lambda4 = 5.18561*x/par1
+    lambda1 = 1.19361*xx/par1
+    lambda2 = 2.38722*xx/par1
+    lambda3 = 2.5928*xx/par1
+    lambda4 = 5.18561*xx/par1
 
-    return A1*E1-A2*E2*(np.cos(lambda1)+np.cos(lambda1)*np.cos(lambda2)+np.sin(lambda1)*np.sin(lambda2))+A3*E3*(np.cos(lambda3)+np.cos(lambda3)*np.cos(lambda4)+np.sin(lambda3)*np.sin(lambda4))+A4*E2*(np.sin(lambda1)-np.cos(lambda2)*np.sin(lambda1)+np.cos(lambda1)*np.sin(lambda2))-A5*E3*(np.sin(lambda3)-np.cos(lambda4)*np.sin(lambda3)+np.cos(lambda3)*np.sin(lambda4))
+    return par3+(A1*E1-A2*E2*(np.cos(lambda1)+np.cos(lambda1)*np.cos(lambda2)+np.sin(lambda1)*np.sin(lambda2))+A3*E3*(np.cos(lambda3)+np.cos(lambda3)*np.cos(lambda4)+np.sin(lambda3)*np.sin(lambda4))+A4*E2*(np.sin(lambda1)-np.cos(lambda2)*np.sin(lambda1)+np.cos(lambda1)*np.sin(lambda2))-A5*E3*(np.sin(lambda3)-np.cos(lambda4)*np.sin(lambda3)+np.cos(lambda3)*np.sin(lambda4)))*np.heaviside(xx,1)
 
 def chi_squared(y_mea, y_mea_std, y_exp):
 # Calculate the chi-squared statistic
@@ -104,34 +107,36 @@ avg_err=raw[1]
 #plt.show()
 #
 chi2_list = []
-for ilink in range(10):
-    for ich in range(256):
+for ilink in range(1):
+    for ich in range(1):
         apulse = avg[ilink][ich]
         apulse_err = avg_err[ilink][ich]
         pmax = np.amax(apulse)
         maxpos = np.argmax(apulse)
-        nbf = 5
-        naf = 20
+        nbf = 10
+        naf = 10
         pbl = apulse[maxpos-nbf]
         a_xx = np.array(range(nbf+naf))*0.5
-        popt, pcov = curve_fit(ResFunc, a_xx, apulse[maxpos-nbf:maxpos+naf]-pbl)
-        apulse_exp = ResFunc(a_xx,popt[0],popt[1])
+        popt, pcov = curve_fit(ResFunc, a_xx, apulse[maxpos-nbf:maxpos+naf],maxfev= 10000,p0=[80000,2,2,1000])
+        apulse_exp = ResFunc(a_xx,popt[0],popt[1],popt[2],popt[3])
         #chi2,pval = chi_squared(apulse[maxpos-nbf:maxpos+naf]-pbl, apulse_exp)
-        chi2 = chi_squared(apulse[maxpos-nbf:maxpos+naf]-pbl, apulse_err[maxpos-nbf:maxpos+naf], apulse_exp)
+        chi2 = chi_squared(apulse[maxpos-nbf:maxpos+naf], apulse_err[maxpos-nbf:maxpos+naf], apulse_exp)
         chi2_list.append(chi2)
-#        
-#        plt.scatter(a_xx, apulse[maxpos-5:maxpos+20]-pbl, c='r')
-#        plt.errorbar(a_xx, apulse[maxpos-5:maxpos+20]-pbl,yerr=apulse_err[maxpos-5:maxpos+20],fmt='o')
-#        xx = np.linspace(0,25,100)*0.5
-#        plt.plot(xx, ResFunc(xx,popt[0],popt[1]))
-#        plt.xlabel('us')
-#        plt.ylabel('ADC')
-#        plt.title('link%d chan%d'%(ilink,ich))
-#        plt.text(8,pmax-1500,'A0=%.2f'%popt[0],fontsize = 15)
-#        plt.text(8,pmax-2500,'tp=%.2f'%popt[1],fontsize = 15)
-#        plt.text(8,pmax-3500,'chi2/dof=%.2f'%chi2,fontsize = 15)
-#        plt.savefig("plots/run%d_link%d_ch%d"%(runno,ilink,ich))
-#        plt.close()
+        
+        plt.scatter(a_xx, apulse[maxpos-nbf:maxpos+naf], c='r')
+        #plt.errorbar(a_xx, apulse[maxpos-5:maxpos+20]-pbl,yerr=apulse_err[maxpos-5:maxpos+20],fmt='o')
+        xx = np.linspace(0,nbf+naf,100)*0.5
+        plt.plot(xx, ResFunc(xx,popt[0],popt[1],popt[2],popt[3]))
+        plt.xlabel('us')
+        plt.ylabel('ADC')
+        plt.title('link%d chan%d'%(ilink,ich))
+        plt.text(8,pmax-1500,'A0=%.2f'%popt[0],fontsize = 15)
+        plt.text(8,pmax-2500,'tp=%.2f'%popt[1],fontsize = 15)
+        plt.text(8,pmax-3500,'t0=%.2f'%popt[2],fontsize = 15)
+        plt.text(8,pmax-4500,'bl=%.2f'%popt[3],fontsize = 15)
+        plt.text(8,pmax-5500,'chi2/dof=%.2f'%chi2,fontsize = 15)
+        #plt.savefig("plots/run%d_link%d_ch%d"%(runno,ilink,ich))
+        #plt.close()
 
-plt.plot(range(2560),chi2_list)
+#plt.plot(range(2560),chi2_list)
 plt.show()
